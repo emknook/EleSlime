4008de12b466533fffffadad801776ca0ca91eba
Marcy <emknook@gmail.com>
2026-04-20 15:14:20 +0200

    Needs some work on the collision, but I think a decent start. found bug, moved the player in multiple places

--- src/main/java/nl/han/jefmk/entities/player/PlayerCollider.java
+++ src/main/java/nl/han/jefmk/entities/player/PlayerCollider.java
@@ -3,13 +3,11 @@ package nl.han.jefmk.entities.player;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.Collider;
-import com.github.hanyaeger.api.entities.Direction;
 import com.github.hanyaeger.api.entities.impl.CircleEntity;
 import javafx.scene.paint.Color;
 import nl.han.jefmk.entities.obstacles.Obstacle;
 import nl.han.jefmk.entities.pickups.Pickup;
 import nl.han.jefmk.surfaces.SurfaceCollider;
-import nl.han.jefmk.surfaces.Tile;
 
 import java.util.List;
 
@@ -40,21 +38,6 @@ public class PlayerCollider extends CircleEntity implements Collided {
 
     private void handleSurfaceCollision(SurfaceCollider surface) {
         player.addTouchingSurfaceDirection(surface.getSurfaceDirection());
-        Tile tile = surface.getTile();
-        switch(surface.getSurfaceDirection()) {
-            case Direction.UP:
-                player.setAnchorLocationY(tile.getAnchorLocation().getY() + tile.getHeight());
-                break;
-            case Direction.DOWN:
-                player.setAnchorLocationY(tile.getAnchorLocation().getY() - player.getHeight());
-                break;
-            case Direction.LEFT:
-                player.setAnchorLocationX(tile.getAnchorLocation().getX() + tile.getWidth());
-                break;
-            case Direction.RIGHT:
-                player.setAnchorLocationX(tile.getAnchorLocation().getX() - player.getWidth());
-                break;
-        }
     }
 
     private void handleObstacleCollision(Obstacle obstacle) {
